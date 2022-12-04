c7fc7be3b89bf806d741a35cf433fa3ab0c5f052
ceejayps <ceejayps1308@gmail.com>
2022-12-04 17:05:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard'
+import Admins from './/pages/Admins'
+import Users from './pages/Users'
+import Transactions from './pages/Transactions'
+import Products from './pages/Products'
+import SideBar from './components/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <SideBar>
+    <Routes>
+      <Route path="/" element={<Dashboard/>}  />
+      <Route path="/dashboard" element={<Dashboard/>}  />
+      <Route path="/admins" element={<Admins/>}  />
+      <Route path="/users" element={<Users/>}  />
+      <Route path="/transactions" element={<Transactions/>}  />
+      <Route path="/products" element={<Products/>}  />
+    </Routes>
+    </SideBar>
+    </BrowserRouter>
   );
 }
 
