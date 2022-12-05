f8ab39c60e82ddc4663958f386b0a080c58ee392
ceejayps <ceejayps1308@gmail.com>
2022-12-04 22:25:39 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { FiBell, FiSearch} from 'react-icons/fi'
+import { BsSquare} from 'react-icons/bs'
+import { BiPaperPlane} from 'react-icons/bi'
+
+
 
 const NavBar = () => {
   return (
-    <div className='navBar'>NavBar</div>
+    <div className='navBar'>
+        <div className='navInput'>
+            <FiSearch/>
+            <input className='navInputField' type="text" />
+        </div>
+        <div className='navRight'>
+      
+        <div className="navIcon offset5px"><FiBell/> </div>
+        <div className="navIcon offset5px"><BsSquare/> </div>
+        <div className="navIcon"><BiPaperPlane/></div>
+           
+            <div className="navAvatar"></div>
+        </div>
+    </div>
   )
 }
 
