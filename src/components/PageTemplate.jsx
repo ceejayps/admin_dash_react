6d953ae4e8c167f76bdef847a11bfc9fad45b9fd
ceejayps <ceejayps1308@gmail.com>
2022-12-08 11:48:20 -0500

    Update PageTemplate.jsx

--- src/components/PageTemplate.jsx
+++ src/components/PageTemplate.jsx
@@ -1,3 +1,4 @@
+import { data } from 'autoprefixer'
 import React from 'react'
 
 
@@ -26,7 +27,6 @@ function PageTemplate(props) {
         {props.data.map((item, index) => (
              <tr>
              <td className='usersData'>
-              
                <img height={'50px'} width={'50px'} src={props.data[index].image}/>
                   <span class="feedTxt">{props.data[index].firstName +" "+props.data[index].lastName}</span>
                </td>
@@ -39,17 +39,20 @@ function PageTemplate(props) {
              <td> </td>
            </tr>
           ))}
-          
-        
-      
-       
-        
-       
       </table>
       
       </div>
       <div className="bottomBar">
-        
+
+        <div className="bottomBarLeft">
+      <select name="10" className="dropdown">
+    <option value="10">10</option>
+    <option value="20">20</option>
+    <option value="50">50</option>
+    <option value="100">100</option>
+  </select>
+  <p>of {props.data.length}</p>
+  </div>
       </div>
     </div>
     
