f46bb6bda72a451cd0605bc4e515f1e1fa0fe137
ceejayps <ceejayps1308@gmail.com>
2022-12-08 16:08:26 -0500

    Update Admins.jsx

--- src/pages/Admins.jsx
+++ src/pages/Admins.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BsAlarm, BsPersonPlus } from "react-icons/bs";
+import PageTemplate from "../components/PageTemplate";
 
 const Admins = () => {
   const [Users, setUsers] = useState([]);
@@ -9,11 +10,11 @@ const Admins = () => {
   const [pages, setPages] = useState([1]);
   const [pageCount, setPagesCount] = useState(1);
 
-  const onPerPageOptionChangeHandler = (event) => {
+  const onPerPageChange = (event) => {
     setLimit(parseInt(event.target.value));
   };
 
-  const onPageOptionChangeHandler = (event) => {
+  const onPageChange = (event) => {
     //setPages(parseInt(event.target.value));
     setSkip(limit* (parseInt(event.target.value)-1))
   };
@@ -27,8 +28,6 @@ const Admins = () => {
         setTotal(data.total);
         
       });
-
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, [limit,skip]);
 
  useEffect(()=>{
@@ -36,16 +35,15 @@ const Admins = () => {
   const elements = []
   for (let index = 0; index < numOfPages; index++) {
     elements.push(index)
-    setPagesCount(numOfPages)
-    setPages(elements)
-    setPagesCount(numOfPages)
   }
- },[total, limit, skip])
-  // const sortedUsers = currentUsers.sort((a, b) => {
-  //           const result = a.lastName.localeCompare(b.lastName);
+  setPagesCount(numOfPages)
+  setPages(elements)
+  setPagesCount(numOfPages)
 
-  //           return result !== 0 ? result : a.firstName.localeCompare(b.firstName);
-  //         })
+},[total, limit, skip])
+
+  const pageName = "Admin"
+  const pageIcon = <BsPersonPlus className="pageButtonIcon" />
 
   const tableFields = [
     "ID",
@@ -58,80 +56,36 @@ const Admins = () => {
     "position",
     "actions",
   ];
+  const list = Users.map((item, index) => (       
+    <tr key={index}>
+      <td>{item.id}</td>
+      <td className="usersData">
+        <img src={item.image} />
+        <span className="feedTxt">
+          {item.firstName + " " + item.lastName}
+        </span>
+      </td>
+      <td>{item.company.name}</td>
+      <td>{item.company.department}</td>
+      <td>
+        {item.address.city + ", " + item.address.state}
+      </td>
+      <td>{item.gender}</td>
+      <td>{item.age}</td>
+      <td>{item.company.title}</td>
+      <td> </td>
+    </tr>
+  ))
 
   return (
-    // <PageTemplate tableFields={tableFields} pageName = {"Admin"} data={Users} icon ={<BsPersonPlus className='pageButtonIcon'/>}/>
-    <div className="templateBody">
-      <div className="headSection">
-        <h2 className="pageTitle">{"Admin"}</h2>
-        <div className="pageButton">
-          {<BsPersonPlus className="pageButtonIcon" />} <p>Create{" Admin"}</p>
-        </div>
-      </div>
-      <hr className="rounded" />
-
-      <div className="tableScrole">
-        <table className="dataTable">
-        <tbody>
-          <tr>
-            {tableFields.map((item, index) => (
-              <th key={index}>{item}</th>
-            ))}
-          </tr>
-          {Users.map((item, index) => (
-            
-            <tr key={index}>
-              <td>{item.id}</td>
-              <td className="usersData">
-                <img src={item.image} />
-                <span className="feedTxt">
-                  {item.firstName + " " + item.lastName}
-                </span>
-              </td>
-              <td>{item.company.name}</td>
-              <td>{item.company.department}</td>
-              <td>
-                {item.address.city + ", " + item.address.state}
-              </td>
-              <td>{item.gender}</td>
-              <td>{item.age}</td>
-              <td>{item.company.title}</td>
-              <td> </td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="bottomBar">
-        <div className="bottomBarLeft">
-          <select
-            name="10"
-            className="dropdown"
-            onChange={onPerPageOptionChangeHandler}
-          >
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </select>
-          <p>per page</p>
-        </div>
-        <div className="bottomBarRight">
-        <select
-            className="dropdown"
-        
-            onChange={onPageOptionChangeHandler}
-          >
-            {
-              pages.map((item,index)=> <option key={index} value={index+1}>{index+1}</option>
-              )
-            }
-            
-          </select>
-          <p>of {pageCount}</p>
-        </div>
-      </div>
-    </div>
+  <PageTemplate
+  pageName={pageName}
+  pageIcon={pageIcon}
+  list={list}
+  tableFields={tableFields}
+  onPageChange={onPageChange}
+  onPerPageChange={onPerPageChange}
+  pages={pages} pageCount={pageCount}/>
   );
 };
 
