cd22ceffee9a4947032c142e7920b684aa8465bb
ceejayps <ceejayps1308@gmail.com>
2022-12-08 15:00:26 -0500

    Update PageTemplate.jsx

--- src/components/PageTemplate.jsx
+++ src/components/PageTemplate.jsx
@@ -2,61 +2,52 @@ import { data } from 'autoprefixer'
 import React from 'react'
 
 
-function PageTemplate(props) {
+function PageTemplate({pageName, pageIcon, tableFields, list, onPageChange, onPerPageChange, pages, pageCount}) {
   return (
-    <div className='templateBody'>
-        <div className='headSection'>
-            <h2 className='pageTitle'>
-                {props.pageName +'s'}
-            </h2>
-
-            <div className='pageButton'>
-                {props.icon} <p>Create {props.pageName}</p>
-            </div>
-           
-        </div>
-        <hr class="rounded"/>
-        
-        <div className="tableScrole">
-      <table className='dataTable'>
-        <tr>
-        {props.tableFields.map((item, index) => (
-            <th>{item}</th>
-          ))}
-        </tr>
-        {props.data.map((item, index) => (
-             <tr>
-             <td className='usersData'>
-               <img height={'50px'} width={'50px'} src={props.data[index].image}/>
-                  <span class="feedTxt">{props.data[index].firstName +" "+props.data[index].lastName}</span>
-               </td>
-               <td>{props.data[index].company.name}</td>
-             <td>{props.data[index].company.department}</td>
-             <td>{props.data[index].address.city+", "+props.data[index].address.state}</td>
-             <td>{props.data[index].gender}</td>
-             <td>{props.data[index].age}</td>
-             <td>{props.data[index].company.title}</td>
-             <td> </td>
-           </tr>
-          ))}
-      </table>
-      
-      </div>
-      <div className="bottomBar">
-
-        <div className="bottomBarLeft">
-      <select name="10" className="dropdown">
-    <option value="10">10</option>
-    <option value="20">20</option>
-    <option value="50">50</option>
-    <option value="100">100</option>
-  </select>
-  <p>of {props.data.length}</p>
-  </div>
+    <div className="templateBody">
+    <div className="headSection">
+      <h2 className="pageTitle">{pageName+"s"}</h2>
+      <div className="pageButton">
+        {pageIcon} <p>Create {pageName}</p>
       </div>
     </div>
-    
-  )
+    <hr className="rounded"/>
+    <div className="tableScrole">
+      <table className="dataTable">
+      <tbody>
+        <tr>
+          {tableFields.map((item, index) => (
+            <th key={index}>{item}</th>
+          ))}
+        </tr>
+        {list}
+        </tbody>
+      </table>
+    </div>
+    <div className="bottomBar">
+      <div className="bottomBarLeft">
+        <select className="dropdown"
+          onChange={onPerPageChange}>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
+        <p>per page</p>
+      </div>
+      <div className="bottomBarRight">
+      <select
+          className="dropdown"
+          onChange={onPageChange}>
+          {
+            pages.map((item,index)=> <option key={index} value={index+1}>{index+1}</option>)
+          }
+        </select>
+        <p>of {pageCount}</p>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default PageTemplate
