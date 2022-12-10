8e631934fccb2f2167f365585a3d0f30a7ac832d
ceejayps <ceejayps1308@gmail.com>
2022-12-09 21:32:33 -0500

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { BsPersonPlus } from 'react-icons/bs'
 
 const Dashboard = () => {
+ 
+
+
+
+
+  const pageName = "Dashboard"
+  const pageIcon = <BsPersonPlus className="pageButtonIcon" />
+
   return (
-    <div>Dashboard</div>
+    <div className="templateBody">
+    <div className="headSection">
+      <h2 className="pageTitle">{pageName+"s"}</h2>
+      <div className="pageButton">
+        {pageIcon} <p>Create {"User"}</p>
+      </div>
+    </div>
+    <hr className="rounded"/>
+   
+ 
+  </div>
   )
 }
 
