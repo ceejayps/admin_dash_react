c4735cf9d19d71b1ed391f38450aab3bb0a33ccb
ceejayps <ceejayps1308@gmail.com>
2022-12-09 22:07:11 -0500

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React,{useEffect, useState} from 'react'
 import { BsPersonPlus } from 'react-icons/bs'
+import { BiLineChartDown, BiLineChart } from 'react-icons/bi'
+import DashboardCard from '../components/DashboardCard'
+<BiLineChartDown/>
 
 const Dashboard = () => {
  
@@ -19,7 +22,11 @@ const Dashboard = () => {
       </div>
     </div>
     <hr className="rounded"/>
-   
+   <div>
+    
+   <DashboardCard title={"Users"} current={"72,345"} previous={"71,233"} isBearish ={!true} data={[]}/>
+
+   </div>
  
   </div>
   )
