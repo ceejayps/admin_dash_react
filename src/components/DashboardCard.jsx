65d051b6d483cc3e6ff9d35ef4bd8a062b4b5d99
ceejayps <ceejayps1308@gmail.com>
2022-12-09 22:07:14 -0500

    Create DashboardCard.jsx

---
+++ src/components/DashboardCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BiLineChartDown, BiLineChart } from 'react-icons/bi'
+const DashboardCard = ({title, current, previous, isBearish, data}) => {
+    const icon = isBearish? <BiLineChart/>:<BiLineChartDown/>
+  return (
+    <div>
+    <div className='dashCardleft'>
+    <p>Tatal{title}</p>
+    <p>{current}</p>
+    <div>{icon} from{previous}</div>
+    </div>
+
+    <div className="dashCardRight">
+        
+        </div>
+
+   </div>
+  )
+}
+
+export default DashboardCard
