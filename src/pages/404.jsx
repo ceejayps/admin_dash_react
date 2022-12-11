fb7ca5eb98b325912d67955d8bd9dda68e194be9
ceejayps <ceejayps1308@gmail.com>
2022-12-11 00:09:22 -0500

    Create 404.jsx

---
+++ src/pages/404.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import '../404.css'
+
+const FourOhFour = () => {
+  return (
+    <div>
+        <div className="Container container-star">
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-1"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+    <div className="star-2"></div>
+  </div>
+  <div className="container-bird">
+    <div className="bird bird-anim">
+      <div className="bird-container">
+        <div className="wing wing-left">
+          <div className="wing-left-top"></div>
+        </div>
+        <div className="wing wing-right">
+          <div className="wing-right-top"></div>
+        </div>
+      </div>
+    </div>
+    <div className="bird bird-anim">
+      <div className="bird-container">
+        <div className="wing wing-left">
+          <div className="wing-left-top"></div>
+        </div>
+        <div className="wing wing-right">
+          <div className="wing-right-top"></div>
+        </div>
+      </div>
+    </div>
+    <div className="bird bird-anim">
+      <div className="bird-container">
+        <div className="wing wing-left">
+          <div className="wing-left-top"></div>
+        </div>
+        <div className="wing wing-right">
+          <div className="wing-right-top"></div>
+        </div>
+      </div>
+    </div>
+    <div className="bird bird-anim">
+      <div className="bird-container">
+        <div className="wing wing-left">
+          <div className="wing-left-top"></div>
+        </div>
+        <div className="wing wing-right">
+          <div className="wing-right-top"></div>
+        </div>
+      </div>
+    </div>
+    <div className="bird bird-anim">
+      <div className="bird-container">
+        <div className="wing wing-left">
+          <div className="wing-left-top"></div>
+        </div>
+        <div className="wing wing-right">
+          <div className="wing-right-top"></div>
+        </div>
+      </div>
+    </div>
+    <div className="bird bird-anim">
+      <div className="bird-container">
+        <div className="wing wing-left">
+          <div className="wing-left-top"></div>
+        </div>
+        <div className="wing wing-right">
+          <div className="wing-right-top"></div>
+        </div>
+      </div>
+    </div>
+    <div className="container-title">
+      <div className="title">
+        <div className="number">4</div>
+        <div className="moon">
+          <div className="face">
+            <div className="mouth"></div>
+            <div className="eyes">
+              <div className="eye-left"></div>
+              <div className="eye-right"></div>
+            </div>
+          </div>
+        </div>
+        <div className="number">4</div>
+      </div>
+      <div className="subtitle">Oops. Looks like you took a wrong turn.</div>
+      <button>Go back</button>
+    </div>
+  </div>
+  </div>
+  )
+}
+
+export default FourOhFour
