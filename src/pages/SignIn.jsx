977f1cf5ab1e0127f36dbb5226695fdcb78c1c04
ceejayps <ceejayps1308@gmail.com>
2022-12-11 00:09:29 -0500

    Create SignIn.jsx

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,54 @@
+import React, { useRef } from "react";
+import { BiLock, BiUser } from "react-icons/bi";
+import { FaLock, FaPersonBooth, FaUser } from "react-icons/fa";
+import "../signIn.css";
+const SignIn = () => {
+  const onSubmit = () => {};
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  return (
+    <div className="signInDiv">
+      <form className="SignUpForm" onSubmit={onSubmit}>
+        <div className="formTitleTextDiv">
+          <label htmlFor="LOGIN" className="formTitleText">
+            LOGIN
+          </label>
+        </div>
+
+        <label htmlFor="email">Email</label>
+
+        <div className="formInputDiv">
+          <BiUser className="formInputIcons" />
+          <input
+            ref={emailRef}
+            type="email"
+            placeholder="email@mail.com"
+            id="email"
+          />
+        </div>
+        <label htmlFor="password">Password</label>
+        <div className="formInputDiv">
+          <BiLock className="formInputIcons" />
+          <input
+            ref={passwordRef}
+            type="password"
+            id="password"
+            placeholder="type your password"
+          />
+        </div>
+        <div className="forgetPasswordButton">
+          <p></p>
+          <label htmlFor="forgetpassword">Forgot Password</label>
+        </div>
+
+        <button className="formSubmitButton" type="submit">
+          {" "}
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
